7def6bb2fbbe26423df3344cdc9a45af0581aef5
ItzYanick <me@yanick.gay>
2022-04-04 13:38:13 +0200

    link to new bot

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceTUD Bot
 // @namespace    https://github.com/PlaceTUD/Soldat
-// @version      22
+// @version      23
 // @description  Der PlaceTUD Bot für die koolsten Kids der TU Darmstadt!
 // @author       Meine liebsten Nerds vom tud_geekhub
 // @match        https://www.reddit.com/r/place/*
@@ -11,8 +11,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/PlaceTUD/Soldat/raw/master/placenlbot.user.js
-// @downloadURL  https://github.com/PlaceTUD/Soldat/raw/master/placenlbot.user.js
+// @updateURL    https://github.com/PlaceTUD/Soldat/raw/master/placetudsoldat.user.js
+// @downloadURL  https://github.com/PlaceTUD/Soldat/raw/master/placetudsoldat.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.xmlHttpRequest
